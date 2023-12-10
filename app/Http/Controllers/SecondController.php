6611cb448b4022fb87b27f0b1a47921cae3ce107
FERNANDO <fgolmos10@gmail.com>
2023-12-10 16:27:39 -0600

    ss

--- app/Http/Controllers/SecondController.php
+++ app/Http/Controllers/SecondController.php
@@ -314,7 +314,7 @@ class SecondController extends Controller
 
     public function RequestPlant(){
         $userid = Auth()->user()->id;
-        $plant=DB::table('plants')->all();
+        $plant=DB::table('plants')->get();
         if($plant){
             return response()->json(['msg'=>"Plantas",'data'=>$plant,'user_id'=>$userid],200);
         }
