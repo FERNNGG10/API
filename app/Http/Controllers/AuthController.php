1c63e40de3e336d8f9398728a28f121a111ad1f9
FERNANDO <fgolmos10@gmail.com>
2023-12-11 15:52:29 -0600

    sdas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -272,7 +272,58 @@ class AuthController extends Controller
         if($user){
             $user->password=Hash::make($request->password);
             $user->save();
-            return redirect()->back()->with('success','Contraseña cambiada correctamente');
+            return '<!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        background-color: #f4f4f4;
+                        padding: 20px;
+                    }
+                    .container {
+                        max-width: 600px;
+                        margin: 0 auto;
+                        background-color: #fff;
+                        padding: 20px;
+                        border-radius: 5px;
+                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    }
+                    h1 {
+                        color: #333;
+                        text-align: center;
+                    }
+                    p {
+                        color: #666;
+                        line-height: 1.6;
+                    }
+                    .btn {
+                        display: block;
+                        width: 200px;
+                        margin: 20px auto;
+                        padding: 10px 15px;
+                        text-align: center;
+                        color: #fff;
+                        background-color: #3AC307;
+                        border: none;
+                        border-radius: 5px;
+                        text-decoration: none;
+                        
+                    }
+                 
+                </style>
+                <title>EMAIL DE ACTIVACIÓN</title>
+            </head>
+            <body>
+                <div class="container">
+                    <h1>¡LifePlant!</h1>
+                    <p class="btn">¡Tu contraseña a sido cambiada exitosamente, puede regresar a la aplicación:)</p>
+                   
+                </div>
+            </body>
+            </html>';
         }
         return redirect()->back()->withErrors(['error' => 'Usuario no encontrado']);
     }
